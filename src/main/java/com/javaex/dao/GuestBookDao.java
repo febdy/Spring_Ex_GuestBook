abfceec2603d69e0c77097242163c639e2b5d3bf
febdy <feb29dy@gmail.com>
2018-02-01 18:02:16 +0900

    dao.getList changed -> mybatis

--- src/main/java/com/javaex/dao/GuestBookDao.java
+++ src/main/java/com/javaex/dao/GuestBookDao.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.javaex.vo.GuestVo;
@@ -15,10 +16,13 @@ import com.javaex.vo.GuestVo;
 @Repository
 public class GuestBookDao {
 
+	@Autowired
+	private SqlSession sqlSession;
+
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;
-	
+
 	public void insert(GuestVo vo) {
 		try {
 			// 1. JDBC 드라이버 (Oracle) 로딩
@@ -29,8 +33,7 @@ public class GuestBookDao {
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 
 			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = "INSERT INTO guestBook "
-						 + "VALUES (seq_guestbook_no.nextval, ?, ?, ?, sysdate)";
+			String query = "INSERT INTO guestBook " + "VALUES (seq_guestbook_no.nextval, ?, ?, ?, sysdate)";
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, vo.getName());
 			pstmt.setString(2, vo.getPassword());
@@ -59,55 +62,7 @@ public class GuestBookDao {
 	}
 
 	public List<GuestVo> getList() {
-		List<GuestVo> gList = new ArrayList<>();
-		GuestVo vo;
-
-		try {
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-
-			// 2. Connection 얻어오기
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-
-			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = "SELECT no, name, password, content, reg_date " 
-						 + " FROM guestbook" 
-						 + " ORDER BY no";
-			pstmt = conn.prepareStatement(query);
-			pstmt.executeQuery();
-			rs = pstmt.getResultSet();
-
-			while (rs.next()) {
-				vo = new GuestVo();
-				vo.setNo(rs.getInt("no"));
-				vo.setName(rs.getString("name"));
-				vo.setPassword(rs.getString("password"));
-				vo.setContent(rs.getString("content"));
-				vo.setDate(rs.getString("reg_date"));
-				gList.add(vo);
-			}
-
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-		}
+		List<GuestVo> gList = sqlSession.selectList("guestbook.getList");
 
 		return gList;
 	}
@@ -122,8 +77,7 @@ public class GuestBookDao {
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 
 			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = "DELETE FROM guestbook "
-						 + "WHERE no = ? AND password = ?";
+			String query = "DELETE FROM guestbook " + "WHERE no = ? AND password = ?";
 			pstmt = conn.prepareStatement(query);
 			pstmt.setInt(1, no);
 			pstmt.setString(2, password);
@@ -148,7 +102,7 @@ public class GuestBookDao {
 				System.out.println("error:" + e);
 			}
 		}
-		
+
 	}
 
 }
